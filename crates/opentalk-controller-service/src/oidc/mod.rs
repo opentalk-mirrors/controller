ff9a10581ba2de7ec9774043f02901324c178706
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2025-12-15 13:42:25 +0100

    refactor(oidc): move OpenIdConnectUserInfo into a separate module

--- crates/opentalk-controller-service/src/oidc/mod.rs
+++ crates/opentalk-controller-service/src/oidc/mod.rs
@@ -5,8 +5,6 @@
 //! Provides OpenID Connect stuff.
 
 use claims::OpenTalkAdditionalClaims;
-use icu_locid::LanguageIdentifier;
-use opentalk_types_common::time::TimeZone;
 use provider::ProviderClient;
 
 mod claims;
@@ -14,6 +12,7 @@ mod http;
 mod introspect_info;
 mod jwt;
 mod oidc_context;
+mod open_id_connect_user_info;
 mod provider;
 mod realm_roles;
 
@@ -21,36 +20,5 @@ pub use claims::{OnlyExpiryClaim, ServiceClaims};
 pub use introspect_info::IntrospectInfo;
 pub use jwt::{VerifyError, decode_token};
 pub use oidc_context::OidcContext;
+pub use open_id_connect_user_info::OpenIdConnectUserInfo;
 pub use realm_roles::RealmRoles;
-
-/// Relevant info returned from `userinfo` endpoint.
-#[derive(Debug)]
-#[must_use]
-pub struct OpenIdConnectUserInfo {
-    /// The users subject identifier, assigned by the OIDC provider
-    pub sub: String,
-    /// The email address
-    pub email: String,
-    /// The users firstname
-    pub firstname: String,
-    /// The last name
-    pub lastname: String,
-    /// The URL to get the avatar from
-    pub avatar_url: Option<String>,
-    /// The locale of the user
-    pub locale: Option<LanguageIdentifier>,
-    /// The timezone of the user
-    pub timezone: Option<TimeZone>,
-    /// The groups
-    pub groups: Vec<String>,
-    /// The phone number
-    pub phone_number: Option<String>,
-    /// The display name
-    pub display_name: Option<String>,
-    /// The tenant id
-    pub tenant_id: Option<String>,
-    /// The tariff id
-    pub tariff_id: Option<String>,
-    /// The tariff status
-    pub tariff_status: Option<String>,
-}
