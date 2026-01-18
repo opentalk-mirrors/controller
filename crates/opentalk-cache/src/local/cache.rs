d4251306456f399e4b6c5df5b01df71dceb837c4
Oleksii Sukhodolskyi <o.sukhodolskyi@opentalk.eu>
2026-01-18 09:04:52 +0100

    chore(cache): test exisiting functionality of the local cache

--- crates/opentalk-cache/src/local/cache.rs
+++ crates/opentalk-cache/src/local/cache.rs
@@ -56,3 +56,75 @@ where
         Ok(())
     }
 }
+
+/// TODO: We could make these tests more generic and move them up to the lib level
+///       to test all CacheStorage implementations (redis, local)
+#[cfg(test)]
+mod tests {
+    use tokio;
+
+    use super::*;
+
+    const DEFAULT_TTL: Duration = Duration::from_millis(300);
+    const CUSTOM_SHORTER_TTL: Duration = Duration::from_millis(100);
+    const CUSTOM_LONGER_TTL: Duration = Duration::from_millis(500);
+
+    async fn setup() -> (Cache<String, String>, String, String) {
+        let cache = Cache::new(DEFAULT_TTL);
+        let key = String::from("key");
+        let value = String::from("value");
+
+        cache.insert(key.clone(), value.clone()).await.unwrap();
+        (cache, key, value)
+    }
+
+    async fn setup_with_entry_ttl(ttl: Duration) -> (Cache<String, String>, String, String) {
+        let cache = Cache::new(DEFAULT_TTL);
+        let key = String::from("key");
+        let value = String::from("value");
+
+        cache
+            .insert_with_ttl(key.clone(), value.clone(), ttl)
+            .await
+            .unwrap();
+        (cache, key, value)
+    }
+
+    #[tokio::test]
+    async fn insertion() {
+        let (cache, original_key, original_value) = setup().await;
+
+        let cached_value = cache.get(&original_key).await.unwrap();
+        assert_eq!(Some(original_value), cached_value);
+    }
+
+    #[tokio::test]
+    async fn expiration_with_default_ttl() {
+        let (cache, original_key, _original_value) = setup().await;
+
+        tokio::time::sleep(DEFAULT_TTL).await;
+
+        let cached_value = cache.get(&original_key).await.unwrap();
+        assert_eq!(cached_value, None);
+    }
+
+    #[tokio::test]
+    async fn expiration_with_shorter_ttl() {
+        let (cache, original_key, _original_value) = setup_with_entry_ttl(CUSTOM_SHORTER_TTL).await;
+
+        tokio::time::sleep(CUSTOM_SHORTER_TTL).await;
+
+        let cached_value = cache.get(&original_key).await.unwrap();
+        assert_eq!(cached_value, None);
+    }
+
+    #[tokio::test]
+    async fn expiration_with_longer_ttl() {
+        let (cache, original_key, _original_value) = setup_with_entry_ttl(CUSTOM_LONGER_TTL).await;
+
+        tokio::time::sleep(DEFAULT_TTL).await;
+
+        let cached_value = cache.get(&original_key).await.unwrap();
+        assert_eq!(cached_value, None);
+    }
+}
