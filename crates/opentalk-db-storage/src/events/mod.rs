e253df90704e8c8a77cd36d22a79e1bac4c3309b
t.spamer <t.spamer@opentalk.eu>
2026-02-13 16:02:42 +0100

    refactor(db): move event shared folders table structs into tables module

--- crates/opentalk-db-storage/src/events/mod.rs
+++ crates/opentalk-db-storage/src/events/mod.rs
@@ -11,14 +11,13 @@ pub use crate::tables::{
     event_exceptions::{EventException, NewEventException, UpdateEventException},
     event_favorites::{EventFavorite, NewEventFavorite},
     event_invites::{EventInvite, NewEventInvite, UpdateEventInvite},
+    event_shared_folders as shared_folders,
     event_training_participation_report_parameter_sets::{
         EventTrainingParticipationReportParameterSet,
         UpdateEventTrainingParticipationReportParameterSet,
     },
     events::{Event, NewEvent, UpdateEvent},
 }; // TODO: rm -f
-
-pub mod shared_folders;
 
 pub struct GetEventsCursor {
     pub from_id: EventId,
--- crates/opentalk-db-storage/src/tables/event_shared_folders/event_shared_folder.rs
+++ crates/opentalk-db-storage/src/tables/event_shared_folders/event_shared_folder.rs
@@ -9,68 +9,10 @@ use opentalk_database::{DbConnection, Result};
 use opentalk_inventory as inventory;
 use opentalk_types_common::{events::EventId, rooms::RoomId};
 
-use super::Event;
-use crate::schema::{event_shared_folders, events};
-
-#[derive(Insertable)]
-#[diesel(table_name = event_shared_folders)]
-pub struct NewEventSharedFolder {
-    pub event_id: EventId,
-    pub path: String,
-    pub write_share_id: String,
-    pub write_url: String,
-    pub write_password: String,
-    pub read_share_id: String,
-    pub read_url: String,
-    pub read_password: String,
-}
-
-impl From<inventory::NewEventSharedFolder> for NewEventSharedFolder {
-    fn from(
-        inventory::NewEventSharedFolder {
-            event_id,
-            path,
-            write_share_id,
-            write_url,
-            write_password,
-            read_share_id,
-            read_url,
-            read_password,
-        }: inventory::NewEventSharedFolder,
-    ) -> Self {
-        Self {
-            event_id,
-            path,
-            write_share_id,
-            write_url,
-            write_password,
-            read_share_id,
-            read_url,
-            read_password,
-        }
-    }
-}
-
-impl NewEventSharedFolder {
-    /// Tries to insert the EventSharedFolder into the database
-    ///
-    /// When yielding a unique constraint violation, None is returned.
-    #[tracing::instrument(err, skip_all)]
-    pub async fn try_insert(self, conn: &mut DbConnection) -> Result<Option<EventSharedFolder>> {
-        let query = self.insert_into(event_shared_folders::table);
-
-        let result = query.get_result(conn).await;
-
-        match result {
-            Ok(event_shared_folders) => Ok(Some(event_shared_folders)),
-            Err(diesel::result::Error::DatabaseError(
-                diesel::result::DatabaseErrorKind::UniqueViolation,
-                ..,
-            )) => Ok(None),
-            Err(e) => Err(e.into()),
-        }
-    }
-}
+use crate::{
+    schema::{event_shared_folders, events},
+    tables::events::Event,
+};
 
 #[derive(Clone, Debug, PartialEq, Eq, Associations, Identifiable, Queryable)]
 #[diesel(table_name = event_shared_folders)]
