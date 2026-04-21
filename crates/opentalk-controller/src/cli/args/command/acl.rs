d3929192ed22d5ddaff565673818942bfd53eb66
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2026-04-21 07:45:31 +0200

    feat: re-introduce dummy `fix-acl` and `acl` commands
    Closes #1360
    
    changelog:ignore

---
+++ crates/opentalk-controller/src/cli/args/command/acl.rs
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+use clap::Parser;
+
+#[derive(Debug, Clone, Parser)]
+pub struct Command {
+    _arbitrary_positional_arguments: Vec<String>,
+}
+
+impl Command {
+    pub fn exec(self) {
+        println!(
+            "The API permission system has been improved, so the `acl` subcommand is no longer necessary."
+        );
+    }
+}
