fb49e621264417fa0de8a8c09c65b57e17b8d967
Sandro Forster <s.forster@opentalk.eu>
2026-04-22 17:23:17 +0200

    feat!: remove obsolete `SignalingRoomId`
    relates to #1341

--- crates/opentalk-signaling-core/src/lib.rs
+++ crates/opentalk-signaling-core/src/lib.rs
@@ -9,7 +9,6 @@ mod expiring_data_hash_map;
 mod object_storage;
 mod participant;
 mod redis_wrapper;
-mod signaling_room_id;
 mod storage_notifier;
 
 pub mod assets;
@@ -21,5 +20,4 @@ pub use expiring_data_hash_map::ExpiringDataHashMap;
 pub use object_storage::{ChunkFormat, ObjectStorage, ObjectStorageError};
 pub use participant::Participant;
 pub use redis_wrapper::{RedisConnection, RedisMetrics};
-pub use signaling_room_id::SignalingRoomId;
 pub use storage_notifier::{NoOpStorageNotifier, RoomServerStorageNotifier, StorageNotifier};
