cb8c36c407f47a1a4d4fc068ecf52fc8dde1b457
Sandro Forster <s.forster@opentalk.eu>
2026-04-22 18:12:12 +0200

    feat!: remove obsolete `Participant` enum
    relates to #1341

--- crates/opentalk-signaling-core/src/lib.rs
+++ crates/opentalk-signaling-core/src/lib.rs
@@ -4,7 +4,6 @@
 
 mod exchange_task;
 mod object_storage;
-mod participant;
 mod redis_wrapper;
 mod storage_notifier;
 
@@ -12,6 +11,5 @@ pub mod assets;
 
 pub use exchange_task::{Error as ExchangeError, ExchangeHandle, ExchangeTask, SubscriberHandle};
 pub use object_storage::{ChunkFormat, ObjectStorage, ObjectStorageError};
-pub use participant::Participant;
 pub use redis_wrapper::{RedisConnection, RedisMetrics};
 pub use storage_notifier::{NoOpStorageNotifier, RoomServerStorageNotifier, StorageNotifier};
