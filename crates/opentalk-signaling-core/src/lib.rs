b4ef29ecaf9d57830abab30106831686322fb780
Sandro Forster <s.forster@opentalk.eu>
2026-04-22 18:10:27 +0200

    feat!: remove obsolete expiring data types
    relates to #1341

--- crates/opentalk-signaling-core/src/lib.rs
+++ crates/opentalk-signaling-core/src/lib.rs
@@ -3,8 +3,6 @@
 // SPDX-License-Identifier: EUPL-1.2
 
 mod exchange_task;
-mod expiring_data;
-mod expiring_data_hash_map;
 mod object_storage;
 mod participant;
 mod redis_wrapper;
@@ -13,8 +11,6 @@ mod storage_notifier;
 pub mod assets;
 
 pub use exchange_task::{Error as ExchangeError, ExchangeHandle, ExchangeTask, SubscriberHandle};
-pub use expiring_data::ExpiringData;
-pub use expiring_data_hash_map::ExpiringDataHashMap;
 pub use object_storage::{ChunkFormat, ObjectStorage, ObjectStorageError};
 pub use participant::Participant;
 pub use redis_wrapper::{RedisConnection, RedisMetrics};
