2e9ad43c35ca20eceaf922d9ff8395b6dd864786
Sandro Forster <s.forster@opentalk.eu>
2026-05-06 16:51:03 +0200

    refactor: move exchange task from `opentalk-signaling-core` to `opentalk-controller-core` crate
    relates to #1341

--- crates/opentalk-signaling-core/src/lib.rs
+++ crates/opentalk-signaling-core/src/lib.rs
@@ -2,8 +2,6 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
-mod exchange_task;
 mod redis_wrapper;
 
-pub use exchange_task::{Error as ExchangeError, ExchangeHandle, ExchangeTask, SubscriberHandle};
 pub use redis_wrapper::{RedisConnection, RedisMetrics};
